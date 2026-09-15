42a9f1093f082a111ed734a8e9fe335b4ebd6d59
Tyas <tyasastrihningkurniasih@gmail.com>
2026-09-15 21:24:07 +0700

    Add files via upload

---
+++ anonymous_func.m
@@ -0,0 +1,22 @@
+% anonymous function
+
+% squared
+squared = @(x) x.^2
+squared(9)
+squared(1:4)
+
+% addition
+addition = @(x, y) x + y
+addition(7, 9)
+
+% subtraction
+substraction = @(x, y) x - y
+substraction(10, 12)
+
+% multiplication
+multiplication = @(x, y) x * y
+multiplication(6, 3)
+
+% division
+division = @(x, y) x / y
+division(1000, 250)
